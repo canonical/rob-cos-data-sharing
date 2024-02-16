5f698c5fe7494c0d1677af73611a50fc31998fde
Giuseppe Barbieri <giuseppe.barbieri@canonical.com>
2024-02-16 09:09:20 +0100

    modify name to rob-cos-data-sharing and improve info

--- snap/local/info.sh
+++ snap/local/info.sh
@@ -1,3 +1,22 @@
 #!/usr/bin/bash
 
-echo "data sharing snap"
+echo "Data sharing snap for on device rob cos snaps.
+      
+This snap offers four content sharing interfaces, to allow snaps on a device that is meant
+to work with the rob-cos ecosystem to easily share data.
+
+It offers the following slots:
+
+common-content-write: allows plugged snaps to write data in $SNAP_COMMON
+common-content-read: allows plugged snaps to read data in $SNAP_COMMON
+data-content-write: allows plugged snaps to write data in $SNAP_DATA
+data-content-read: allows plugged snaps to read data in $SNAP_DATA
+
+Usage:
+
+Connect to common-content-write as follows:
+sudo snap connect rob-cos-snap:common-content-write rob-cos-data-sharing:common-content-write
+
+Connect to common-content-read as follows:
+sudo snap connect rob-cos-snap:common-content-read rob-cos-data-sharing:common-content-read
+"
